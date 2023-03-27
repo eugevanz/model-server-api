454ee77fb49df87d467731d8c170206fd317af28
Eugene Van Zyl <eugevanz@gmail.com>
2023-03-27 18:34:33 +0200

    Finally, database operations are working

--- utilities/make_prediction.py
+++ utilities/make_prediction.py
@@ -1,16 +1,15 @@
 import time
 from os import getenv
 
-# import joblib as jb
+import joblib as jb
 import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
 from requests import get
-from sqlalchemy import create_engine
-from sqlalchemy.sql import text
-# import sqlite3
+from sqlalchemy import create_engine, text
 
-# import make_tables
+# from sqlalchemy.sql import text
+# import psycopg2 as pg2
 
 load_dotenv()
 api_key_id = getenv('LUNO_API_KEY_ID')
@@ -18,9 +17,6 @@ api_key_secret = getenv('LUNO_API_KEY_SECRET')
 
 since = int(time.time() * 1000) - 3 * 60 * 59 * 1000
 duration = 300
-
-# Create the SQLAlchemy engine
-engine = create_engine(getenv('RENDER_SQL_EXT'), echo=True)
 
 
 def cleaning(pair):
@@ -42,7 +38,6 @@ def cleaning(pair):
         return tminuses + volumes
 
     df.rename(columns={'timestamp': 'Time'}, inplace=True)
-    # df.set_index('Time', inplace=True)
 
     df.Time = df.Time.astype(int)
     df.open = df.open.astype(float)
@@ -63,52 +58,74 @@ def cleaning(pair):
 
     df['signal'] = np.where(df.change > 0, True, False)
 
-    # ind_vars = df[['tminus_1', 'tminus_2', 'tminus_3', 'vol_1', 'vol_2', 'vol_3', 'ema12_diff']]
+    ind_vars = df[['tminus_1', 'tminus_2', 'tminus_3', 'vol_1', 'vol_2', 'vol_3', 'ema12_diff']]
 
-    # model = jb.load('assets/model.pkl')
-    # df['prediction'] = list(model.predict(ind_vars))
+    model = jb.load('assets/model.pkl')
+    df['prediction'] = list(model.predict(ind_vars))
 
     derivatives = pd.DataFrame(
-        {
-            'late_close': [df.close.tolist()[-1]],
-            'late_ema': [df.ema12.tolist()[-1]],
-            # 'late_signal': [df.prediction.tolist()[-1] and (late_ema > late_close)],
-            'max_high': [df.high.idxmax()],
-            'min_low': [df.low.idxmin()],
-            'max_close': [df.close.idxmax()],
-            'min_close': [df.close.idxmin()],
-            'avg_close': [df.close.mean()]
-        }
+        [{
+            'late_close': df.close.tolist()[-1],
+            'late_ema': df.ema12.tolist()[-1],
+            'late_signal': df.prediction.tolist()[-1] and (df.ema12.tolist()[-1] < df.close.tolist()[-1]),
+            'max_high': df.high.idxmax(),
+            'min_low': df.low.idxmin(),
+            'max_close': df.close.idxmax(),
+            'min_close': df.close.idxmin(),
+            'avg_close': df.close.mean()
+        }]
     )
 
     return df, derivatives
 
 
-def add_to_database(pair):
-    df, derivatives = cleaning(pair)
-    # Write the dataframe to the tables
-    with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
-        df.to_sql(pair, con=conn, if_exists='append', index=False)
-        derivatives.to_sql(pair + 'deriv', con=conn, if_exists='append', index=False)
+# def connect():
+#     connection = engine.connect()
+#     connection.autocommit = True
+#
+#     return connection
+
+def add_to_database(df, tbl_name):
+    status = 'add_to_database FAILED'
+    try:
+        # Create the SQLAlchemy engine
+        engine = create_engine(getenv('RENDER_SQL_EXT'), echo=True, future=True)
+        # Define the query to insert data into the table
+        cursor_result = df.to_sql(tbl_name, con=engine, if_exists='replace', index=False)
+        status = f'add_to_database Succeeded!, {cursor_result}'
+    except Exception as error:
+        print(error)
+
+    return status
 
 
-def select_db(pair):
-    # Define the query to select data from the table
-    with engine.connect().execution_options(autocommit=True) as conn:
-        # derivatives = pd.read_sql(text(f'''SELECT * FROM {pair}deriv'''), con=conn)
-        candles = pd.read_sql(text(f'''SELECT * FROM {pair}'''), con=conn)
-        # print(derivatives)
-        print(candles)
+def select_db(query):
+    result = 'Nothing Returned'
+    try:
+        # Create the SQLAlchemy engine
+        engine = create_engine(getenv('RENDER_SQL_EXT'), echo=True, future=True)
+        # Define the query to select data from the table
+        with engine.connect() as connection:
+            result = connection.execute(text(query))
+            result = pd.DataFrame(result.fetchall())
+
+        # df = pd.read_sql(f'select * from {tbl_name}', con=engine)
+    except Exception as error:
+        print(error)
+
+    return result
 
 
 if __name__ == '__main__':
-    try:
-        # make_tables.create(engine)
-        # add_to_database('XBTZAR')
-        add_to_database('ETHZAR')
-        # select_db('XBTZAR')
-        select_db('ETHZAR')
-    except Exception as e:
-        print(e)
-    finally:
-        engine.dispose()
+    xbtzar_df, xbtzar_deriv = cleaning('XBTZAR')
+    ethzar_df, ethzar_deriv = cleaning('ETHZAR')
+
+    print(add_to_database(xbtzar_df, 'xbtzar_df'))
+    print(add_to_database(ethzar_df, 'ethzar_df'))
+    print(add_to_database(xbtzar_deriv, 'xbtzar_deriv'))
+    print(add_to_database(ethzar_deriv, 'ethzar_deriv'))
+
+    # print(select_db('select * from xbtzar_df'))
+    # print(select_db('select * from ethzar_df'))
+    # print(select_db('select * from xbtzar_deriv'))
+    # print(select_db('select * from ethzar_deriv'))
--- utilities/make_tables.py
+++ utilities/make_tables.py
@@ -1,13 +1,11 @@
 from sqlalchemy import MetaData, Table, Column, Integer, Boolean, Float
-from sys import argv
 
 
 # Create the tables
-def create(engine):
+def create_candles_table(engine, pair):
     metadata_obj = MetaData()
-
-    Table(
-        'XBTZARcandles',
+    table = Table(
+        pair,
         metadata_obj,
         Column('Time', Integer, primary_key=True),
         Column('open', Float),
@@ -24,42 +22,18 @@ def create(engine):
         Column('vol_3', Float),
         Column('ema12', Float),
         Column('ema12_diff', Float),
-        Column('signal', Boolean)
-    )
-    Table(
-        'ETHZARcandles',
-        metadata_obj,
-        Column('Time', Integer, primary_key=True),
-        Column('open', Float),
-        Column('close', Float),
-        Column('high', Float),
-        Column('low', Float),
-        Column('volume', Float),
-        Column('change', Float),
-        Column('tminus_1', Float),
-        Column('vol_1', Float),
-        Column('tminus_2', Float),
-        Column('vol_2', Float),
-        Column('tminus_3', Float),
-        Column('vol_3', Float),
-        Column('ema12', Float),
-        Column('ema12_diff', Float),
-        Column('signal', Boolean)
+        Column('signal', Boolean),
+        Column('prediction', Boolean),
     )
 
-    Table(
-        'ETHZARderivatives',
-        metadata_obj,
-        Column('late_close', Integer, primary_key=True),
-        Column('late_ema', Float),
-        Column('max_high', Integer),
-        Column('min_low', Float),
-        Column('max_close', Float),
-        Column('min_close', Float),
-        Column('avg_close', Float)
-    )
-    Table(
-        'XBTZARderivatives',
+    metadata_obj.create_all(engine)
+    return table
+
+
+def create_deriv_table(engine, pair):
+    metadata_obj = MetaData()
+    table = Table(
+        f'{pair}_deriv',
         metadata_obj,
         Column('late_close', Integer, primary_key=True),
         Column('late_ema', Float),
@@ -71,3 +45,4 @@ def create(engine):
     )
 
     metadata_obj.create_all(engine)
+    return table
